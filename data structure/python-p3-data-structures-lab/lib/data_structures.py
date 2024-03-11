9fc370e01fb455f85bb54b01220c5b9a95347fd2
LisonOCipher <lisoncipher5@gmail.com>
2024-03-11 13:24:05 +0300

    Add files via upload

---
+++ data structure/python-p3-data-structures-lab/lib/data_structures.py
@@ -0,0 +1,61 @@
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    },
+]
+
+def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
+
+def get_spiciest_foods(spicy_foods):
+    return [ food for food in spicy_foods if food["heat_level"] > 5]
+
+def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+def print_spiciest_foods(spicy_foods):
+  for food in spicy_foods:
+        heat_level = food["heat_level"]
+        emojis = "🌶" * heat_level
+        if heat_level > 5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {emojis}")
+
+
+def get_average_heat_level(spicy_foods):
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    total_foods = len(spicy_foods)
+    if total_foods > 0:
+        return total_heat / total_foods
+    else:
+        return 0
+
+def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+new_spicy_food = {
+    "name": "Griot",
+    "cuisine": "Haitian",
+    "heat_level": 10,
+}
+
+create_spicy_food(spicy_foods, new_spicy_food)
+print("create_spicy_food")
